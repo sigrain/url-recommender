4fd11710376fb89a86b00f69adabd9088336381c
Rin Ohsugi <z5452066@ad.unsw.edu.au>
2024-04-13 14:46:09 +1000

    Save credentials into env file

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp, getApps, FirebaseApp } from "firebase/app";
+import { getFirestore, Firestore, addDoc, getDocs, collection } from "firebase/firestore";
 import {
     getAuth,
     Auth,
@@ -12,12 +13,12 @@ import firebase from "firebase/compat/app";
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcjZBb4cQtq8IeCYPhoQHAzwA1WBQPSyI",
-  authDomain: "url-recommender.firebaseapp.com",
-  projectId: "url-recommender",
-  storageBucket: "url-recommender.appspot.com",
-  messagingSenderId: "564275718901",
-  appId: "1:564275718901:web:96725935a85ece30f5b3bc"
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID
 };
 
 const app = initializeApp(firebaseConfig);
@@ -46,8 +47,29 @@ export const signup = async(email: string, password: string) => {
 
 export const signin = async(email: string, password: string) => {
     try {
-        await signInWithEmailAndPassword(auth, email, password);
+        let userCredentials = await signInWithEmailAndPassword(auth, email, password);
+        user = userCredentials.user;
     } catch(error) {
         return error;
     }
+}
+
+export const signout = async() => {
+    try {
+        await signOut(auth);
+    } catch(error) {
+        return error;
+    }
+}
+
+export { firebaseApp, auth, user };
+
+const d = firebase.firestore();
+
+export const addUser = async(name: string, email: string) => {
+    const userRef = d.collection('users').doc(user?.uid);
+    userRef.set({
+        name: name,
+        email: email,
+    });
 }
