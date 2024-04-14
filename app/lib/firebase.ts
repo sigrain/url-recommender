82e821eed9164abf1bf3dc1aeff098832f6f3adf
Rin Ohsugi <z5452066@ad.unsw.edu.au>
2024-04-14 16:27:08 +1000

    Renew config and fix addUser function

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,87 +1,3 @@
-// import { initializeApp, getApps, FirebaseApp } from "firebase/app";
-// import { getFirestore, Firestore, addDoc, getDocs, collection } from "firebase/firestore";
-// import {
-//     getAuth,
-//     Auth,
-//     signInWithEmailAndPassword,
-//     signOut,
-//     onAuthStateChanged,
-//     User,
-//     createUserWithEmailAndPassword,
-// } from "firebase/auth";
-// import firebase from "firebase/compat/app";
-// import 'firebase/compat/firestore';
-
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_APIKEY,
-//   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_PROJECTID,
-//   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-//   appId: process.env.NEXT_PUBLIC_APPID
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// let firebaseApp: FirebaseApp;
-// let auth: Auth;
-// let firestore: Firestore;
-// let user: User | null = null;
-
-// if (typeof window !== "undefined" && !getApps().length) {
-//     firebaseApp = initializeApp(firebaseConfig);
-//     firebase.initializeApp(firebaseConfig);
-//     auth = getAuth();
-//     onAuthStateChanged(auth, (userData) => {
-//         user = userData;
-//     })
-// }
-
-// firebaseApp = initializeApp(firebaseConfig);
-// firebase.initializeApp(firebaseConfig);
-// auth = getAuth();
-// onAuthStateChanged(auth, (userData) => {
-//     user = userData;
-// })
-
-// export const signup = async(email: string, password: string) => {
-//     try {
-//         const auth = getAuth()
-//         await createUserWithEmailAndPassword(auth, email, password);
-//     } catch(error) {
-//         return error;
-//     }
-// }
-
-// export const signin = async(email: string, password: string) => {
-//     try {
-//         let userCredentials = await signInWithEmailAndPassword(auth, email, password);
-//         user = userCredentials.user;
-//         return user;
-//     } catch(error) {
-//         return error;
-//     }
-// }
-
-// export const signout = async() => {
-//     try {
-//         await signOut(auth);
-//     } catch(error) {
-//         return error;
-//     }
-// }
-
-// export { firebaseApp, auth, user };
-
-// const d = firebase.firestore();
-
-// export const addUser = async(name: string, email: string) => {
-//     const userRef = d.collection('users').doc(user?.uid);
-//     userRef.set({
-//         name: name,
-//         email: email,
-//     });
-// }
 "use client"
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, addDoc, collection } from "firebase/firestore";
@@ -103,7 +19,7 @@ const auth = getAuth(app);
 const firestore = getFirestore(app);
 
 // Sign up function
-export const signup = async (email, password) => {
+export const signup = async (email: string, password: string) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     console.log({res})
@@ -142,20 +58,10 @@ export const signout = async () => {
 
 // Add user to Firestore
 export const addUser = async (email: string, username: string) => {
-  try {
-    console.log(email, username);
-    const myCollection = collection(firestore, 'users');
-    const myDocumentData = {
-        email: email,
-        icon: "",
-        username: username,
-    };
-    const newDocRef = await addDoc(myCollection, myDocumentData);
-    console.log({newDocRef});
-  } catch (error) {
-    console.error("Add user error:", error);
-    throw error;
-  }
+  await addDoc(collection(firestore, 'users'), {
+    email: email,
+    username: username
+  })
 };
 
 export const addPost = async (pp: string, summary: string, title: string, url: string, userid: string, username: string) => {
@@ -177,11 +83,13 @@ export const addPost = async (pp: string, summary: string, title: string, url: s
   };
 
 // Listen for auth state changes
+/*
 export const listenForAuthChanges = (callback) => {
   onAuthStateChanged(auth, (user) => {
     callback(user);
   });
 };
+*/
 
 // Exporting instances
 export { auth, firestore };
